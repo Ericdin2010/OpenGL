d8f6ae2eefb4be3228fb5cb83194e8b4ac439318
Ericdin2010 <121679629+Ericdin2010@users.noreply.github.com>
2024-05-04 23:25:19 +0300

    now finally drawing pipe I need

--- OpenGL pygame pipe.py
+++ OpenGL pygame pipe.py
@@ -20,30 +20,33 @@ except ImportError:
     import pygame
     import math
 
-verticies = (
-    (0.5, -0.5, -0.5),
-    (0.5, 0.5, -0.5),
-    (-0.5, 0.5, -0.5),
-    (-0.5, -0.5, -0.5),
-    (0.5, -0.5, 0.5),
-    (0.5, 0.5, 0.5),
-    (-0.5, -0.5, 0.5),
-    (-0.5, 0.5, 0.5)
-    )
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+verticies = ((0.5, -0.5, -0.5),
+             (0.5, 0.5, -0.5),
+             (-0.5, 0.5, -0.5),
+             (-0.5, -0.5, -0.5),
+             (0.5, -0.5, 0.5),
+             (0.5, 0.5, 0.5),
+             (-0.5, -0.5, 0.5),
+             (-0.5, 0.5, 0.5))
+edges = ((0,1),
+         (0,3),
+         (0,4),
+         (2,1),
+         (2,3),
+         (2,7),
+         (6,3),
+         (6,4),
+         (6,7),
+         (5,1),
+         (5,4),
+         (5,7))
+
+def ReadFile():
+    global fileData
+    if os.path.isfile('file.txt') is True:
+        with open('file.txt', 'r') as file:
+            data = file.read().replace('\n', '')
+            fileData = list(data)
 
 def Cube():
     glBegin(GL_LINES)
@@ -52,44 +55,86 @@ def Cube():
             glVertex3fv(verticies[vertex])
     glEnd()
 
-def Pipe():
-    p = 0.125
-    d = 0.5
-    num_slices = 24
-    height = 1
-    radius = 0.25
-    GREEN = (0.18, 0.51, 0.27)
-    glBegin(GL_QUADS)
+def XYZLines():
+    global RED, GREEN, BLUE, WHITE
+    glBegin(GL_LINES)
+    glColor3fv(RED)
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((0.5, 0, 0))
+    glEnd()
+    glBegin(GL_LINES)
     glColor3fv(GREEN)
-    for surface in range(0, 24):
-        glVertex3fv((p, -d, p))
-        glVertex3fv((p, -d, -p))
-        glVertex3fv((-p, -d, -p))
-        glVertex3fv((-p, -d, p))
-
-    for i in range(num_stacks):
-        h = i / num_stacks
-        top_color = colorsys.hsv_to_rgb(h, 1.0, 1.0)
-        bottom_color = colorsys.hsv_to_rgb(h + 0.1, 1.0, 1.0)
-
-        glBegin(GL_QUAD_STRIP)
-    
-    for j in range(num_slices + 1):
-            angle = j * 2 * 3.14159 / num_slices
-            x = radius * math.cos(angle)
-            y = radius * math.sin(angle)
-            glVertex3f(x, y, h * height)
-            glVertex3f(x, y, (h + 1 / num_stacks) * height)
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((0, 0.5, 0))
+    glEnd()
+    glBegin(GL_LINES)
+    glColor3fv(BLUE)
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((0, 0, 0.5))
+    glColor3fv(WHITE)
     glEnd()
 
+def Pipe():
+    global RED, GREEN, BLUE, WHITE, fileData
+    radiusMax = 0.25
+    radiusMin = 0.20
+    thickness = 0.05
+    height = 1
+    slices = 16
+    stacks = 1
+    surrentStack = 0
+    lastStack = 0
+    pi = 3.141592653
+    deltaAngle = pi / slices * 2
+    angle = 0
+    SquareSize = math.sin(pi / slices)
+    xShift = SquareSize / 2
+    yShift = 0
+    zShift = 0
+
+    for i in range(stacks):
+        surrentStack = surrentStack + SquareSize
+        xOut = 0
+        yOut = 0
+        xLastOut = 0
+        yLastOut = 0
+        glBegin(GL_QUADS)
+        for i in range(slices):
+            if fileData[i]:
+                glColor3fv(RED)
+            else:
+                glColor3fv(GREEN)
+            angle = angle + deltaAngle
+            xOut = xOut + math.cos(angle) * SquareSize
+            yOut = yOut + math.sin(angle) * SquareSize
+
+            glVertex3fv((xLastOut + xShift, yLastOut + yShift, lastStack + zShift))
+            glVertex3fv((xLastOut + xShift, yLastOut + yShift, surrentStack + zShift))
+            glVertex3fv((xOut + xShift, yOut + yShift, surrentStack + zShift))
+            glVertex3fv((xOut + xShift, yOut + yShift, lastStack + zShift))
+            xLastOut = xOut
+            yLastOut = yOut
+        glEnd()
+        lastStack = surrentStack
+
+    glColor3fv(WHITE)
+
+RED = (215 / 256, 10 / 256, 10 / 256)
+GREEN = (40 / 256, 220 / 256, 70 / 256)
+BLUE = (18 / 256, 47 / 256, 170 / 256)
+WHITE = (1, 1, 1)
+BLACK = (0, 0, 0)
+fileData = []
+
 def main():
-    
+    ReadFile()
     pressed = [False, False, False, False, False]
     x_rotation = 0
     y_rotation = 0
     pygame.init()
     display = (800, 800)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    glEnable(GL_DEPTH_TEST)
     glMatrixMode(GL_PROJECTION)
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
     glMatrixMode(GL_MODELVIEW)
@@ -129,44 +174,18 @@ def main():
             y_rotation += 2.0
         if pressed[4]:
             y_rotation -= 2.0
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
-        glTranslatef(0.0, 0.0, -3)
+        glTranslatef(0.0, 0.0, -5)
         glRotatef(x_rotation, 1, 0, 0)
         glRotatef(y_rotation, 0, 1, 0)
         
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        XYZLines()
         Pipe()
+        #Cube()
         pygame.display.flip()
         pygame.time.wait(10)
 
 if __name__ == "__main__":
     main()
-
-##def draw_pipe(radius=0.25, thickness=0.05, height=1, num_slices=64, num_stacks=64):
-##    quadric = gluNewQuadric()
-##
-##    for i in range(num_stacks):
-##        h = i / num_stacks
-##        top_color = colorsys.hsv_to_rgb(h, 1.0, 1.0)
-##        bottom_color = colorsys.hsv_to_rgb(h + 0.1, 1.0, 1.0)
-##
-##        glBegin(GL_QUAD_STRIP)
-##
-##        for j in range(num_slices + 1):
-##            angle = j * 2 * 3.14159 / num_slices
-##            x = radius * math.cos(angle)
-##            y = radius * math.sin(angle)
-##
-##            glColor3f(*top_color)
-##            glVertex3f(x, y, h * height)
-##
-##            glColor3f(*bottom_color)
-##            glVertex3f(x, y, (h + 1 / num_stacks) * height)
-##
-##        glEnd()
-        #glLoadIdentity() is a function in pyOpenGL that resets the
-        #current modelview matrix to the identity matrix. This means
-        #that it effectively resets any transformations that have been
-        #applied to the scene, essentially setting it back to its original state.
-        #It is commonly used at the beginning of a drawing function to ensure
-        #that the scene is drawn without any previous transformations affecting it.
